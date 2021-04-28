4740f581f2f476ddb1035ccc85b325429c42ade9
Michael Täge <mictaege@googlemail.com>
2021-04-28 14:01:11 +0200

    @Feature annotation for grouping scenarios.

--- src/test/java/com/github/mictaege/arete/CalculatorGherkinStyleSpec.java
+++ src/test/java/com/github/mictaege/arete/CalculatorGherkinStyleSpec.java
@@ -15,127 +15,121 @@ class CalculatorGherkinStyleSpec {
         calculator = new Calculator();
     }
 
-    @Scenario()
-    class Should_Add_5_To_10 {
-        int a, b, c;
-        @Given(1) void five() {
-            a = 5;
-        }
-        @Given(2) void ten() {
-            b = a * 2;
+    @Feature class Addition {
+
+        @Scenario class Should_Add_5_To_10 {
+            int a, b, c;
+            @Given(1) void five() {
+                a = 5;
+            }
+            @Given(2) void ten() {
+                b = a * 2;
+            }
+
+            @When void addingTogether() {
+                c = calculator.add(a, b);
+            }
+
+            @Then(1) void theResultShouldBeCorrect() {
+                assertThat(c, is(15));
+            }
+
+            @Then(2) void not_be_an_equal_number() {
+                assertThat(c % 2, is(1));
+            }
         }
 
-        @When void addingTogether() {
-            c = calculator.add(a, b);
+        @Scenario class ShouldAddTwoNumbers {
+            int d;
+            @When(seq = 1, step = 1)
+            void addingTwoNumbersTogether() {
+                d = calculator.add(10, 5);
+            }
+
+            @Then(seq = 1, step = 1)
+            void theResultShouldBeCorrect() {
+                assertThat(d, is(15));
+                assertThat(d, lessThan(16));
+                assertThat(d % 2, is(1));
+            }
+
+            @When(seq = 2, step = 1)
+            void addingTwoOtherNumbers() {
+                d = calculator.add(10, 10);
+            }
+
+            @Then(seq = 2, step = 1)
+            void theOtherResultShouldBeCorrect() {
+                assertThat(d, is(20));
+            }
+
+            @Then(seq = 2, step = 2)
+            void notBeToLarge() {
+                assertThat(d, lessThan(21));
+            }
+
+            @Then(seq = 2, step = 3)
+            void beAnEqualNumber() {
+                assertThat(d % 2, is(0));
+            }
         }
 
-        @Then(1)
-        void theResultShouldBeCorrect() {
-            assertThat(c, is(15));
+        @Scenario class ShouldAddThreeNumbers {
+            int a, b, c, d;
+
+            @Given(1) void aFirstNumber() {
+                a = 5;
+            }
+
+            @Given(2) void aSecondNumber() {
+                b = 7;
+            }
+
+            @Given(3) void aThirdNumber() {
+                c = 3;
+            }
+
+            @When void addingTogether() {
+                d = calculator.add(a, b, c);
+            }
+
+            @Then(1) void theResultShouldBeCorrect() {
+                assertThat(d, is(15));
+            }
+
+            @Then(2) void notBeToLarge() {
+                assertThat(d, lessThan(16));
+            }
+
+            @Then(3) void notBeAnEqualNumber() {
+                assertThat(d % 2, is(1));
+            }
         }
 
-        @Then(2)
-        void not_be_an_equal_number() {
-            assertThat(c % 2, is(1));
-        }
     }
 
-    @Scenario(desc = "Scenario: Should subtract two numbers leading to a negative result")
-    class ShouldSubtractTwoNumbers {
-        int a;
-        @Given(step = 1, desc = "Given the number 5") void aFirstNumber() {
-            a = 5;
-        }
+    @Feature
+    class Subtracting {
+        @Scenario(desc = "Scenario: Should subtract two numbers leading to a negative result")
+        class ShouldSubtractTwoNumbers {
+            int a;
+            @Given(step = 1, desc = "Given the number 5") void aFirstNumber() {
+                a = 5;
+            }
 
-        int b;
-        @Given(step = 2, desc = "And the number 10") void aSecondNumber() {
-            b = 10;
-        }
+            int b;
+            @Given(step = 2, desc = "And the number 10") void aSecondNumber() {
+                b = 10;
+            }
 
-        int c;
-        @When void subtractingFromEachOther() {
-            c = calculator.subtract(a, b);
-        }
+            int c;
+            @When void subtractingFromEachOther() {
+                c = calculator.subtract(a, b);
+            }
 
-        @Then
-        void theResultShouldBeCorrect() {
-            assertThat(c, is(-5));
-        }
-    }
-
-    @Scenario
-    class ShouldAddTwoNumbers {
-        int d;
-        @When(seq = 1, step = 1)
-        void addingTwoNumbersTogether() {
-            d = calculator.add(10, 5);
-        }
-
-        @Then(seq = 1, step = 1)
-        void theResultShouldBeCorrect() {
-            assertThat(d, is(15));
-            assertThat(d, lessThan(16));
-            assertThat(d % 2, is(1));
-        }
-
-        @When(seq = 2, step = 1)
-        void addingTwoOtherNumbers() {
-            d = calculator.add(10, 10);
-        }
-
-        @Then(seq = 2, step = 1)
-        void theOtherResultShouldBeCorrect() {
-            assertThat(d, is(20));
-        }
-
-        @Then(seq = 2, step = 2)
-        void notBeToLarge() {
-            assertThat(d, lessThan(21));
-        }
-
-        @Then(seq = 2, step = 3)
-        void beAnEqualNumber() {
-            assertThat(d % 2, is(0));
-        }
-    }
-
-    @Scenario
-    class ShouldAddThreeNumbers {
-        int a, b, c, d;
-
-        @Given(1)
-        void aFirstNumber() {
-            a = 5;
-        }
-
-        @Given(2)
-        void aSecondNumber() {
-            b = 7;
-        }
-
-        @Given(3)
-        void aThirdNumber() {
-            c = 3;
-        }
-
-        @When
-        void addingTogether() {
-            d = calculator.add(a, b, c);
-        }
-
-        @Then(1)
-        void theResultShouldBeCorrect() {
-            assertThat(d, is(15));
-        }
-
-        @Then(2)
-        void notBeToLarge() {
-            assertThat(d, lessThan(16));
-        }
-
-        @Then(3)
-        void notBeAnEqualNumber() {
-            assertThat(d % 2, is(1));
+            @Then void theResultShouldBeCorrect() {
+                assertThat(c, is(-5));
+            }
         }
     }
 
